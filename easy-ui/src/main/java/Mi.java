e9fa4765298148ad04b4b1a067c5ac0afdf9ada0
Error <2583588839@qq.com>
2018-08-25 13:49:46 +0800

    Mi

---
+++ easy-ui/src/main/java/Mi.java
@@ -0,0 +1,8 @@
+public class Mi {
+
+    public static void main(String[] arg0){
+        String a ="dong 字母";
+		ss
+        
+    }
+}
--- easy-ui/src/main/java/Tj.java
+++ easy-ui/src/main/java/Tj.java
@@ -1,13 +1,36 @@
+import java.util.HashMap;
+
 public class Tj {
-
     public static void main(String[] arg0){
-
-        String a="abcdefg";
-        String s = a.substring(2, 3);
-        System.out.println(s);
-        System.out.println("mi");
-        System.out.println("hai");
-        System.out.println("d");
-        System.out.println("e");
-    };
+        String a="sjd  ASDF  中文字符";
+        Integer alength=a.length();
+        char[] chars = a.toCharArray();
+        HashMap<String, Integer> map = new HashMap<>();
+        int count1=0;
+        int count2=0;
+        int count3=0;
+        int count4=0;
+        for(int i=0;i<alength;i++){
+            boolean b = Character.isAlphabetic(chars[i]);
+            if (b){
+                count1=count1+1;
+            }
+            boolean c = Character.isDigit(chars[i]);
+            if (c){
+                count2=count2+1;
+            }
+            boolean d = Character.isSpaceChar(chars[i]);
+            if (d){
+                count3=count3+1;
+            }
+            if(chars[i]>='a'&&chars[i]<='z'||(chars[i]>='A'&&chars[i]<='Z')){
+                count4++;
+            }
+        }
+        map.put("字符数 ",count1);
+        map.put("数字 ",count2);
+        map.put("空格数 ",count3);
+        map.put("字母数",count4);
+        System.out.println(map);
+    }
 }
